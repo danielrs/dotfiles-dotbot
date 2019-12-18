ecae83ae95d28132406191a14d0af4443819bf53
Daniel Rivas <ers.daniel+dev@gmail.com>
2019-12-18 14:05:45 -0600

    Added __fish_describe_command function as a temporal workaround in catalina

---
+++ fish/functions/__fish_describe_command.fish
@@ -0,0 +1,6 @@
+# Empty describe command:
+# see: https://github.com/fish-shell/fish-shell/issues/6270
+
+function __fish_describe_command --description 'Empty describe command due to https://github.com/fish-shell/fish-shell/issues/6270'
+end
+
