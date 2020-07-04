e21e658fbd057c27f73e85425030350abb8d2c38
Daniel Rivas <danielrs@users.noreply.github.com>
2020-07-04 12:00:18 -0500

    Added Ansible installation script

---
+++ scripts/install-ansible.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+# See: http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -e -u
+
+if command -v ansible &>/dev/null; then
+    echo "Ansible already installed"
+    exit 0
+fi
+
+if ! command -v pip &>/dev/null; then
+    echo "pip not found"
+    exit 1
+fi
+
+pip install ansible
+
