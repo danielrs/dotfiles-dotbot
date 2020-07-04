d2dc05ab3b3e7e8a0eda65ad863fdd1725a95474
Daniel Rivas <danielrs@users.noreply.github.com>
2020-07-04 12:03:15 -0500

    Install/upgrades ansible instead of just intalling

--- scripts/install-ansible.sh
+++ scripts/install-ansible.sh
@@ -3,15 +3,15 @@
 # See: http://redsymbol.net/articles/unofficial-bash-strict-mode/
 set -e -u
 
-if command -v ansible &>/dev/null; then
-    echo "Ansible already installed"
-    exit 0
-fi
+# if command -v ansible &>/dev/null; then
+#     echo "Ansible already installed"
+#     exit 0
+# fi
 
 if ! command -v pip &>/dev/null; then
     echo "pip not found"
     exit 1
 fi
 
-pip install ansible
+pip install --upgrade ansible
 
